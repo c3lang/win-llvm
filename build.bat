5facfc34a18c8b6ae479e0f29533579c98543536
Christian Buttner <christian@cbuttner.de>
2024-07-16 16:09:39 +0200

    Build compiler-rt from the runtimes directory.

--- build.bat
+++ build.bat
@@ -17,4 +17,12 @@ cmake --build . --target install %CMAKE_BUILD_FLAGS%
 
 cd %THIS_DIR%
 
+mkdir runtimes\build
+cd runtimes\build
+cmake .. %RUNTIMES_CMAKE_CONFIGURE_FLAGS%
+cmake --build . %CMAKE_BUILD_FLAGS%
+cmake --build . --target install %CMAKE_BUILD_FLAGS%
+
+cd %THIS_DIR%
+
 7z a -t7z %GITHUB_WORKSPACE%\%LLVM_RELEASE_FILE% %LLVM_RELEASE_NAME%
